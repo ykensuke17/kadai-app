652de09ff253e9f8ec84a190711423ac078f8818
302A-15\cre <a>
2024-11-26 16:09:14 +0900

    11/26 バグ２・３修正した

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -45,6 +45,7 @@ class LoginController extends Controller
      */
     public function logout()
     {
+        Session::flush();
         return redirect('/login');
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -17,6 +17,7 @@ class User extends Model
     public function posts()
     {
         return Post::where('user', $this->id)
+            ->where('is_deleted', false)
             ->orderBy('created_at', 'desc')
             ->get();
     }
